2b724e631f84b6305f6770286c1566eea45adfda
fucci-dev <heather@fucci.dev>
2024-12-09 19:35:44 -0800

    no fines2

--- eVDMm/rules.mk
+++ eVDMm/rules.mk
@@ -2,15 +2,9 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
-AUTO_SHIFT_ENABLE = yes
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = yes
 TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
-
-
-
-
-
 KEY_LOCK_ENABLE = yes
